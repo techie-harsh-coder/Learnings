4afbb491512d6379bc722605e8b09f453f5fff63
Harsh Dubey <harsh9569dubey@gmail.com>
2024-11-14 12:28:36 +0530

    Add files via upload

---
+++ JS/String_Methods.js
@@ -0,0 +1,44 @@
+// let str = "anOnYmoUs";
+let str = "Rahul Pratap";
+let str2 = '123-456-789-000-111';
+
+// Unicode values --->
+// 'A' -> 65;
+// 'Z' -> 90;
+// 'a' -> 97;
+// 'z' -> 122;
+// '0' -> 48;
+// '9' -> 57
+
+// String Methods --->
+// console.log(str.split(Seprator)); // return array of string
+// console.log(str.split(' '));
+// console.log(str.startsWith('a'));
+// console.log(str.endsWith("mous"));
+// console.log(str.toUpperCase());
+// console.log(str.includes('z'));
+// console.log(str2.length);  // returns length of string
+// console.log(str2.padEnd(30,'0'));
+// console.log(str.concat(str2,'jvacjvsajcv'));
+// console.log(str);
+// console.log(str2.replaceAll('0','2').replaceAll('-','').replaceAll('1',''));
+// console.log(str.slice(strtIndex,endingIndex));
+// console.log(str.slice(3,8));        // ending index is excluded and supports -ve indexing
+// console.log(str.substring(3,5));  // same as slice but doesn't support -ve indexing
+// console.log(str.repeat(3));
+// console.log(str.trim()+'1');   // remove whitespaces from start and end
+// console.log(str.trimStart() + "1");      // remove whitespaces from start
+// console.log(str.trimEnd() + "1");  // remove whitespaces from end
+// console.log(str.charCodeAt());       // returns unicode value of characters
+// console.log(str.at(4));      // support -ve indexing and return undefined for invalid position
+// console.log(str.charAt(10));       // doesn't support -ve indexing and return empty string for invalid position  
+// console.log(str.indexOf('s'));      // return -1 for invalid character
+// console.log(str.lastIndexOf('n'));   // return -1 for invalid character
+
+// Exercise -->
+// convert aNonYmOUs ---> Anonymous
+
+
+
+
+
